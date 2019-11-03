78ce66a1a56722c3da37b8ec708b0ab0af8481c8
N Sivanoly <nsivanoly@gmail.com>
2019-11-03 19:18:58 +0530

    heroku app fixes

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 /*
  Define paths for Express Configs
@@ -96,6 +97,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is started on 3000');
+app.listen(port, () => {
+    console.log('Server is started on ' + port);
 });
